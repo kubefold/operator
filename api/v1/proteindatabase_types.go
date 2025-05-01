83a97758332e0dec682523a200347eafb92ae4da
Mateusz Woźniak <matisiek11@gmail.com>
2025-05-01 12:30:22 +0200

    `refactor: simplify ProteinDatabase types and remove unused log observer functionality`

--- api/v1/proteindatabase_types.go
+++ api/v1/proteindatabase_types.go
@@ -1,27 +1,8 @@
-/*
-Copyright 2025 Mateusz Woźniak <wozniakmat@student.agh.edu.pl>.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package v1
 
 import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
-// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
 
 type ProteinDatabaseVolume struct {
 	// +optional
@@ -35,7 +16,7 @@ type ProteinDatabaseVolume struct {
 	Selector *metav1.LabelSelector `json:"selector,omitempty" protobuf:"bytes,4,opt,name=selector"`
 }
 
-type ProteinDatabaseDatasets struct {
+type ProteinDatabaseDatasetSelection struct {
 	MHYClusters bool `json:"mgy_clusters_2022_05"`
 	BFD         bool `json:"bfd-first_non_consensus_sequences"`
 	UniRef90    bool `json:"uniref90_2022_05"`
@@ -47,16 +28,9 @@ type ProteinDatabaseDatasets struct {
 	RFam        bool `json:"rfam_14_9_clust_seq_id_90_cov_80_rep_seq"`
 }
 
-// ProteinDatabaseSpec defines the desired state of ProteinDatabase.
 type ProteinDatabaseSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
-
-	// Foo is an example field of ProteinDatabase. Edit proteindatabase_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
-
-	Datasets ProteinDatabaseDatasets `json:"datasets,omitempty"`
-	Volume   ProteinDatabaseVolume   `json:"volume,omitempty"`
+	Datasets ProteinDatabaseDatasetSelection `json:"datasets,omitempty"`
+	Volume   ProteinDatabaseVolume           `json:"volume,omitempty"`
 }
 
 type ProteinDatabaseDownloadStatus string
@@ -68,17 +42,33 @@ const (
 	ProteinDatabaseDownloadStatusFailed      ProteinDatabaseDownloadStatus = "Failed"
 )
 
-// ProteinDatabaseStatus defines the observed state of ProteinDatabase.
-type ProteinDatabaseStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+type ProteinDatabaseDatasetDownloadProgress struct {
+	DownloadStatus ProteinDatabaseDownloadStatus `json:"downloadStatus,omitempty"`
+	DownloadSpeed  string                        `json:"downloadSpeed,omitempty"`
+	Progress       string                        `json:"progress,omitempty"`
+	Size           string                        `json:"size,omitempty"`
+	TotalSize      string                        `json:"totalSize,omitempty"`
+	LastUpdate     metav1.Time                   `json:"lastUpdate"`
+}
 
+type ProteinDatabaseDatasetDownloadStatus struct {
+	MHYClusters ProteinDatabaseDatasetDownloadProgress `json:"mgy_clusters_2022_05,omitempty"`
+	BFD         ProteinDatabaseDatasetDownloadProgress `json:"bfd-first_non_consensus_sequences,omitempty"`
+	UniRef90    ProteinDatabaseDatasetDownloadProgress `json:"uniref90_2022_05,omitempty"`
+	UniProt     ProteinDatabaseDatasetDownloadProgress `json:"uniprot_all_2021_04,omitempty"`
+	PDB         ProteinDatabaseDatasetDownloadProgress `json:"pdb_2022_09_28_mmcif_files,omitempty"`
+	PDBSeqReq   ProteinDatabaseDatasetDownloadProgress `json:"pdb_seqres_2022_09_28,omitempty"`
+	RNACentral  ProteinDatabaseDatasetDownloadProgress `json:"rnacentral_active_seq_id_90_cov_80_linclust,omitempty"`
+}
+
+type ProteinDatabaseStatus struct {
 	DownloadStatus ProteinDatabaseDownloadStatus `json:"downloadStatus,omitempty"`
 	DownloadSpeed  string                        `json:"downloadSpeed,omitempty"`
 	VolumeName     string                        `json:"volumeName"`
 	Progress       string                        `json:"progress,omitempty"`
 	Size           string                        `json:"size,omitempty"`
 	TotalSize      string                        `json:"totalSize,omitempty"`
+	LastUpdate     metav1.Time                   `json:"lastUpdate,omitempty"`
 }
 
 // +kubebuilder:object:root=true
@@ -91,7 +81,6 @@ type ProteinDatabaseStatus struct {
 // +kubebuilder:printcolumn:name="Volume",type=string,JSONPath=`.status.volumeName`
 // +kubebuilder:printcolumn:name="Age",type="date",JSONPath=".metadata.creationTimestamp"
 
-// ProteinDatabase is the Schema for the proteindatabases API.
 type ProteinDatabase struct {
 	metav1.TypeMeta   `json:",inline"`
 	metav1.ObjectMeta `json:"metadata,omitempty"`
@@ -102,7 +91,6 @@ type ProteinDatabase struct {
 
 // +kubebuilder:object:root=true
 
-// ProteinDatabaseList contains a list of ProteinDatabase.
 type ProteinDatabaseList struct {
 	metav1.TypeMeta `json:",inline"`
 	metav1.ListMeta `json:"metadata,omitempty"`
--- internal/observer/log_observer.go
+++ internal/observer/log_observer.go
@@ -1,21 +1,12 @@
 package observer
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
-	"fmt"
-	"io"
 	"time"
 
-	"k8s.io/api/batch/v1"
-	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/kubernetes"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
-
-	datav1 "github.com/kubefold/operator/api/v1"
 )
 
 var log = logf.Log.WithName("log_observer")
@@ -25,12 +16,9 @@ type LogObserver interface {
 }
 
 type logObserver struct {
-	client            client.Client
-	kubeClient        kubernetes.Interface
-	stopCh            chan struct{}
-	lastSizeMap       map[string]int64
-	lastTimestampMap  map[string]time.Time
-	downloadSpeedsMap map[string]float64
+	client     client.Client
+	kubeClient kubernetes.Interface
+	stopCh     chan struct{}
 }
 
 type LogEntry struct {
@@ -46,12 +34,9 @@ type LogEntry struct {
 
 func NewLogObserver(c client.Client, kubeClient kubernetes.Interface) LogObserver {
 	return &logObserver{
-		client:            c,
-		kubeClient:        kubeClient,
-		stopCh:            make(chan struct{}),
-		lastSizeMap:       make(map[string]int64),
-		lastTimestampMap:  make(map[string]time.Time),
-		downloadSpeedsMap: make(map[string]float64),
+		client:     c,
+		kubeClient: kubeClient,
+		stopCh:     make(chan struct{}),
 	}
 }
 
@@ -87,276 +72,5 @@ func (o *logObserver) run(ctx context.Context) {
 }
 
 func (o *logObserver) updateProteinDatabaseStatus(ctx context.Context) error {
-	proteinDBList := &datav1.ProteinDatabaseList{}
-	if err := o.client.List(ctx, proteinDBList); err != nil {
-		return fmt.Errorf("failed to list ProteinDatabase resources: %w", err)
-	}
-
-	for i := range proteinDBList.Items {
-		proteinDB := &proteinDBList.Items[i]
-		if err := o.updateSingleProteinDBStatus(ctx, proteinDB); err != nil {
-			log.Error(err, "Failed to update ProteinDatabase status",
-				"name", proteinDB.Name, "namespace", proteinDB.Namespace)
-		}
-	}
-
-	return nil
-}
-
-func (o *logObserver) updateSingleProteinDBStatus(ctx context.Context, proteinDB *datav1.ProteinDatabase) error {
-	jobList := &v1.JobList{}
-	labelSelector := client.MatchingLabels{
-		"app.kubernetes.io/instance":   proteinDB.Name,
-		"app.kubernetes.io/managed-by": "kubefold-operator",
-	}
-	if err := o.client.List(ctx, jobList, labelSelector, client.InNamespace(proteinDB.Namespace)); err != nil {
-		return fmt.Errorf("failed to list jobs for ProteinDatabase %s: %w", proteinDB.Name, err)
-	}
-
-	var totalSize, totalBytes int64
-	allCompleted := true
-	datasetCount := 0
-	resourceKey := fmt.Sprintf("%s/%s", proteinDB.Namespace, proteinDB.Name)
-
-	for i := range jobList.Items {
-		job := &jobList.Items[i]
-
-		if job.Status.CompletionTime == nil {
-			allCompleted = false
-		}
-
-		podList := &corev1.PodList{}
-		podLabelSelector := client.MatchingLabels{
-			"job-name": job.Name,
-		}
-		if err := o.client.List(ctx, podList, podLabelSelector, client.InNamespace(job.Namespace)); err != nil {
-			log.Error(err, "Failed to list pods for job", "job", job.Name)
-			continue
-		}
-
-		for j := range podList.Items {
-			pod := &podList.Items[j]
-			if pod.Status.Phase != corev1.PodRunning && pod.Status.Phase != corev1.PodSucceeded {
-				continue
-			}
-
-			req := o.kubeClient.CoreV1().Pods(pod.Namespace).GetLogs(pod.Name, &corev1.PodLogOptions{
-				Container: "downloader",
-				Follow:    false,
-				TailLines: int64Ptr(100),
-			})
-
-			podLogs, err := req.Stream(ctx)
-			if err != nil {
-				log.Error(err, "Failed to stream logs for pod", "pod", pod.Name)
-				continue
-			}
-
-			jobSize, jobTotal, err := o.processLogStreamForTotals(podLogs)
-			podLogs.Close()
-
-			if err != nil {
-				log.Error(err, "Error processing log stream")
-				continue
-			}
-
-			if jobTotal > 0 {
-				totalSize += jobSize
-				totalBytes += jobTotal
-				datasetCount++
-			}
-		}
-	}
-
-	if datasetCount > 0 {
-		proteinDBCopy := proteinDB.DeepCopy()
-
-		var progressStr string
-		if totalBytes > 0 {
-			progressPercent := float64(totalSize) / float64(totalBytes) * 100
-			progressStr = fmt.Sprintf("%.1f%%", progressPercent)
-		} else {
-			progressStr = "0%"
-		}
-
-		sizeStr := humanReadableSize(totalSize)
-		totalSizeStr := humanReadableSize(totalBytes)
-
-		downloadSpeedStr := o.calculateDownloadSpeed(resourceKey, totalSize)
-
-		if allCompleted && datasetCount > 0 {
-			proteinDBCopy.Status.DownloadStatus = datav1.ProteinDatabaseDownloadStatusCompleted
-			downloadSpeedStr = ""
-		} else if totalSize > 0 {
-			proteinDBCopy.Status.DownloadStatus = datav1.ProteinDatabaseDownloadStatusDownloading
-		} else {
-			proteinDBCopy.Status.DownloadStatus = datav1.ProteinDatabaseDownloadStatusNotStarted
-			downloadSpeedStr = ""
-		}
-
-		proteinDBCopy.Status.Progress = progressStr
-		proteinDBCopy.Status.Size = sizeStr
-		proteinDBCopy.Status.TotalSize = totalSizeStr
-		proteinDBCopy.Status.DownloadSpeed = downloadSpeedStr
-
-		if proteinDBCopy.Status.Progress != proteinDB.Status.Progress ||
-			proteinDBCopy.Status.Size != proteinDB.Status.Size ||
-			proteinDBCopy.Status.TotalSize != proteinDB.Status.TotalSize ||
-			proteinDBCopy.Status.DownloadStatus != proteinDB.Status.DownloadStatus ||
-			proteinDBCopy.Status.DownloadSpeed != proteinDB.Status.DownloadSpeed {
-
-			if err := o.client.Status().Update(ctx, proteinDBCopy); err != nil {
-				if errors.IsConflict(err) {
-					log.V(1).Info("Conflict updating ProteinDatabase status, will retry",
-						"name", proteinDB.Name, "namespace", proteinDB.Namespace)
-					return nil
-				}
-				return fmt.Errorf("failed to update ProteinDatabase status: %w", err)
-			}
-
-			log.Info("Updated ProteinDatabase status",
-				"name", proteinDB.Name,
-				"namespace", proteinDB.Namespace,
-				"progress", progressStr,
-				"size", sizeStr,
-				"totalSize", totalSizeStr,
-				"downloadSpeed", downloadSpeedStr)
-		}
-	}
-
-	return nil
-}
-
-func (o *logObserver) calculateDownloadSpeed(resourceKey string, currentSize int64) string {
-	now := time.Now()
-	lastSize, sizeExists := o.lastSizeMap[resourceKey]
-	lastTimestamp, timeExists := o.lastTimestampMap[resourceKey]
-
-	defer func() {
-		o.lastSizeMap[resourceKey] = currentSize
-		o.lastTimestampMap[resourceKey] = now
-	}()
-
-	if !sizeExists || !timeExists || lastSize >= currentSize {
-		if !sizeExists || lastSize != currentSize {
-			o.downloadSpeedsMap[resourceKey] = 0
-			return "0 B/s"
-		}
-		return formatSpeed(o.downloadSpeedsMap[resourceKey])
-	}
-
-	elapsed := now.Sub(lastTimestamp).Seconds()
-	if elapsed <= 0 {
-		return formatSpeed(o.downloadSpeedsMap[resourceKey])
-	}
-
-	sizeChange := currentSize - lastSize
-	speedBytesPerSec := float64(sizeChange) / elapsed
-
-	if prevSpeed, ok := o.downloadSpeedsMap[resourceKey]; ok && prevSpeed > 0 {
-		speedBytesPerSec = 0.7*prevSpeed + 0.3*speedBytesPerSec
-	}
-
-	o.downloadSpeedsMap[resourceKey] = speedBytesPerSec
-
-	return formatSpeed(speedBytesPerSec)
-}
-
-func formatSpeed(bytesPerSec float64) string {
-	const (
-		_        = iota
-		KB int64 = 1 << (10 * iota)
-		MB
-		GB
-		TB
-	)
-
-	unit := "B/s"
-	value := bytesPerSec
-
-	switch {
-	case bytesPerSec >= float64(TB):
-		unit = "TB/s"
-		value = bytesPerSec / float64(TB)
-	case bytesPerSec >= float64(GB):
-		unit = "GB/s"
-		value = bytesPerSec / float64(GB)
-	case bytesPerSec >= float64(MB):
-		unit = "MB/s"
-		value = bytesPerSec / float64(MB)
-	case bytesPerSec >= float64(KB):
-		unit = "KB/s"
-		value = bytesPerSec / float64(KB)
-	}
-
-	return fmt.Sprintf("%.2f %s", value, unit)
-}
-
-func (o *logObserver) processLogStreamForTotals(logStream io.ReadCloser) (size int64, total int64, err error) {
-	scanner := bufio.NewScanner(logStream)
-
-	var latestSize, latestTotal int64
-	foundEntry := false
-
-	for scanner.Scan() {
-		line := scanner.Text()
-		var logEntry LogEntry
-		if err := json.Unmarshal([]byte(line), &logEntry); err != nil {
-			continue
-		}
-
-		if logEntry.Type != "download" {
-			continue
-		}
-
-		latestSize = logEntry.Size
-		latestTotal = logEntry.Total
-		foundEntry = true
-	}
-
-	if err := scanner.Err(); err != nil {
-		return 0, 0, fmt.Errorf("error reading log stream: %w", err)
-	}
-
-	if !foundEntry {
-		return 0, 0, nil
-	}
-
-	return latestSize, latestTotal, nil
-}
-
-func int64Ptr(i int64) *int64 {
-	return &i
-}
-
-func humanReadableSize(bytes int64) string {
-	const (
-		_        = iota
-		KB int64 = 1 << (10 * iota)
-		MB
-		GB
-		TB
-	)
-
-	unit := ""
-	value := float64(bytes)
-
-	switch {
-	case bytes >= TB:
-		unit = "TB"
-		value = float64(bytes) / float64(TB)
-	case bytes >= GB:
-		unit = "GB"
-		value = float64(bytes) / float64(GB)
-	case bytes >= MB:
-		unit = "MB"
-		value = float64(bytes) / float64(MB)
-	case bytes >= KB:
-		unit = "KB"
-		value = float64(bytes) / float64(KB)
-	default:
-		unit = "B"
-	}
-
-	return fmt.Sprintf("%.2f %s", value, unit)
+	panic("implement me")
 }
